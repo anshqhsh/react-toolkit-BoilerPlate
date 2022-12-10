73974aa4fc9b4c4a518bb7f3d8d53bf2a6d6a44b
JOON HYUK LEE <anshqhsh@naver.com>
2022-12-11 00:52:29 +0900

    eslint, airbnb setting

--- src/page/App/App.jsx
+++ src/page/App/App.jsx
@@ -4,19 +4,6 @@ import { Counter } from '../../components/Counter';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState(0);
-
-  useEffect(() => {
-    console.log(value);
-    setValue(`ss`);
-    check();
-  }, [value]);
-
-  const check = () => {
-    console.log('check');
-    setValue('dd');
-  };
-
   return (
     <div className="App">
       <header className="App-header">
